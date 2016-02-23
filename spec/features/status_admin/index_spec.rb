5636644bccc60ffbd139f7c4dd00292253f3a97c
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-02-23 18:12:41 +0000

    First test for the client admin page

--- spec/features/status_admin/index_spec.rb
+++ spec/features/status_admin/index_spec.rb
@@ -1,8 +1,14 @@
-require 'rails_helper'
-
 feature 'Priority old allocations page' do
   scenario 'Login' do
-    visit "/"
-    binding.pry
+    when_i_login_as_an_existing_user
+    then_i_should_arrive_on_the_status_page
+  end
+
+  def when_i_login_as_an_existing_user
+    visit clients_path
+  end
+
+  def then_i_should_arrive_on_the_status_page
+    expect(body).to include('Liste des clients')
   end
 end
