73e9b784de5c9fd9a2bc82939cda1b0ff6f02116
MINETTE Alexandre <minette.alexandre@gmail.com>
2016-04-12 14:24:35 +0100

    cleaning

--- app/controllers/status_admin_controller.rb
+++ app/controllers/status_admin_controller.rb
@@ -4,16 +4,19 @@ class StatusAdminController < ApplicationController
   def index
     @client = Client.find_by!(unique_id: params[:unique_id])
     @messages = SmsMessage::get_message_list(@client)
-    if request.request_method === 'POST'
-      message_id = params[:client_event][:event_name].to_sym
-      event = SmsMessage::get_message(message_id: message_id, client: @client)
-      e = ClientEvent.new(event)
-      e.client = @client
-      e.save!
-      if e.last_message === true
-        @client.processed = true
-        @client.save!
-      end
+    create_client(request) if request.request_method === 'POST'
+  end
+
+  private
+  def create_client(request)
+    message_id = params[:client_event][:event_name].to_sym
+    event = SmsMessage::get_message(message_id: message_id, client: @client)
+    e = ClientEvent.new(event)
+    e.client = @client
+    e.save!
+    if e.last_message === true
+      @client.processed = true
+      @client.save!
     end
   end
 
