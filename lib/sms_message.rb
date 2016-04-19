0644804f62451b3c2438f2aea4dd1cf7bfb0c9e5
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-04-19 21:43:40 +0100

    Removing legacy code

--- lib/sms_message.rb
+++ lib/sms_message.rb
@@ -1,37 +1,4 @@
 class SmsMessage
-  def self.get_message_list(client)
-    hello = "Microdeo - Bonjour #{client.full_name}.\n"
-    horaires = "\nOuvert du Mardi au Samedi, de 9h à 12h et de 14h à 19h, sauf 18h le Samedi."
-    end_message = "\n"\
-      "MESSAGE AUTOMATIQUE. MERCI DE NE PAS REPONDRE."
-    {
-      repair_done: {event_code:'repair_done', event_name: 'Réparation terminée',
-                    message: "#{hello}" \
-                    "Votre #{client.product_full_name} est maintenant réparé, vous pouvez venir le chercher à tout moment au magasin."\
-                    "#{horaires}"\
-                    "\n#{end_message}",
-                    last_message: true
-                    },
-      repair_in_progress: {event_code: 'repair_in_progress', event_name: 'Réparation en cours',
-                           message: "#{hello}"\
-                           "Votre #{client.product_full_name} est en cours de réparation, nous vous tiendrons informés de l'avancement de la réparation.\n#{end_message}"},
-      problem_delay: { event_code: 'problem_delay', event_name: 'Problème de réception des pièces',
-                       message: "#{hello}"\
-                       "Nous avons des problèmes de réception des pièces pour votre #{client.product_full_name} et la réparation est actuellement retardée.\n"\
-                       "Vous serez tenu informé de l'évolution de la réparation.\n"\
-                       "#{end_message}"},
-
-      prise_en_charge: { event_code: 'prise_en_charge', event_name: 'Prise en charge du produit',
-                         message: "#{hello}"\
-                         "Votre #{client.product_full_name} est pris en charge.\n"\
-                         "Vous serez tenu informé de l'évolution de la réparation.\n"\
-                         "#{end_message}",
-                         show_on_list: false
-                         }
-
-    }
-  end
-
   def self.send_sms(client, message)
       return false if client.has_landline_phone?
       user = client.user
