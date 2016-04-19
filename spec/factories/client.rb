34be1936213dddeca5deb91a630b93414f79efcc
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-04-19 16:01:08 +0100

    Adding user_id and adding proper last_name/first_name

--- spec/factories/client.rb
+++ spec/factories/client.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :client do
-    first_name Faker::Name.name
-    last_name Faker::Name.name
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name.upcase
     address "#{Faker::Address.street_name} #{Faker::Address.street_address}"
     postal_code Faker::Address.postcode
     city Faker::Address.city
@@ -11,6 +11,8 @@ FactoryGirl.define do
     product_state :excellent
     brand 'Apple'
     product_name 'Iphone'
+
+    user_id User.first.id
 
     trait :with_invalid_phone do
       phone '0000'
