c828dcfd23e79f99bbd8585ba721c3e58b26ac00
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-04-25 16:35:50 +0100

    Moving the creation in its own file

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -23,24 +23,15 @@ class ClientController < ApplicationController
 
   def new
     if request.request_method === 'POST'
-      c = Client.new(authorize(params))
-      c.user = current_user
-      prise_en_charge = SmsMessage::get_message({
-        message_id: :prise_en_charge,
-        client: c
-      })
-      event = ClientEvent.new(prise_en_charge)
-      c.client_events.push(event)
-      event.send_sms
-      c.save!
-      redirect_to client_path(c.unique_id)
+      client = Client::Creator.create(authorize(params).merge(user: current_user))
+      redirect_to client_path(client.unique_id)
     else
       @client = Client.new
     end
   rescue ActiveRecord::RecordInvalid, \
          Exceptions::SMSMessageFailure => e
     @client = Client.new(authorize(params))
-    @errors = [e.message]
+    add_notification_error(e.message)
     render :action=>'new'
   end
 
@@ -49,7 +40,6 @@ class ClientController < ApplicationController
       attrs = authorize(params)
       client = Client.find_by!(unique_id: params[:unique_id])
       client.assign_attributes(attrs)
-      client.user = current_user
       client.save!
     end
     @client = Client.find_by!(unique_id: params[:unique_id])
