eae8d82a94d328f23aecd3f0989033f66c5d9cc4
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-04-29 21:33:35 +0100

    getting notifications by company

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,6 @@ class NotificationsController < ApplicationController
   include Authenticated
 
   def index
+    @notifications = ClientEvent.from_company(current_user.company).order(created_at: :desc)
   end
 end
--- app/models/client_event.rb
+++ app/models/client_event.rb
@@ -1,5 +1,10 @@
 class ClientEvent < ActiveRecord::Base
   belongs_to :client
+  has_one :company, :through => :client
+
+  def self.from_company(company)
+    joins(:client).where(clients: { company_id: company })
+  end
 
   def send_sms!
     if self.sms_sent != true
@@ -7,5 +12,4 @@ class ClientEvent < ActiveRecord::Base
       self.sms_sent = true
     end
   end
-
 end
