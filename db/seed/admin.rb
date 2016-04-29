e77330e43e4c4f1f5ddb1c22035d6c5c493b4dfb
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-04-29 21:24:53 +0100

    moving the twillo config to its own file

--- db/seed/admin.rb
+++ db/seed/admin.rb
@@ -3,10 +3,7 @@ puts "[admin user]"
 def admin_user
   {
     email: ENV['ADMIN_EMAIL'],
-    password: '12345678', # this is not used and replaced just after
-    twillo_account_sid: ENV['TWILLO_ACCOUNT_SID'],
-    twillo_auth_token: ENV['TWILLO_AUTH_TOKEN'],
-    twillo_root_phone: ENV['TWILLO_ROOT_PHONE']
+    password: '12345678'
   }
 end
 
