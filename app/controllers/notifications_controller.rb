394ad5d6034038faa83cd8d182ac48ea1c8c5221
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-05-06 01:24:31 +0100

    Redability

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,7 @@ class NotificationsController < ApplicationController
   include Authenticated
 
   def index
-    @notifications = ClientEvent.from_company(current_user.company).order(created_at: :desc)
+    @notifications = ClientEvent.from_company(current_user.company)
+                                .order(created_at: :desc)
   end
 end
