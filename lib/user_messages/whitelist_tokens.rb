51769f1fff3f5dd9fc20ed83c7b74e4a630bbdaf
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-05-20 20:32:27 +0100

    Adding the tokens in color

--- lib/user_messages/whitelist_tokens.rb
+++ lib/user_messages/whitelist_tokens.rb
@@ -1,16 +1,27 @@
 class UserMessages::WhitelistTokens
-  def self.get
+  def self.token_list
     [
       '<%= company.name %>',
-      '<%= client.full_name %>'
+      '<%= client.full_name %>',
+      '<%= client.product_full_name %>'
     ]
   end
 
   def self.valid?(message)
     message = message.dup
-    get.each do |token|
+    token_list.each do |token|
       message.gsub!(token, '_replaced_')
     end
     message.index('<%') == nil
+  end
+
+  def self.i18n_replace(message)
+    token_list.each do |token|
+      replacement = '<span class="user-message-token">' +
+                    I18n.t("user_message.token.#{token.delete('<>=% ')}") +
+                    '</span>'
+      message.gsub!(token, replacement)
+    end
+    message
   end
 end
