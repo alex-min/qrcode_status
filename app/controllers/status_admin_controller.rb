5f952464838b67b50a652ca51c18c76819ac0896
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-06-17 01:23:40 +0100

    looks nicer

--- app/controllers/status_admin_controller.rb
+++ app/controllers/status_admin_controller.rb
@@ -4,12 +4,12 @@ class StatusAdminController < ApplicationController
   def index
     @client = Client.find_by!(unique_id: params[:unique_id])
     @messages = UserMessage.where(user: current_user)
-    sent_event(request) if request.request_method == 'POST'
+    send_event(request) if request.request_method == 'POST'
   end
 
   private
 
-  def sent_event(request)
+  def send_event(request)
     message_id = params[:client_event][:event_name].to_sym
     SmsMessage::send_sms_by_code(@client, message_id)
   end
