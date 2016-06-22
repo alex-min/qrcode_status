e3f06cbe7d914e7e17edb347b4cddcae9885091f
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-06-22 04:11:24 +0100

    every user must have a default company

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save :set_default_company
   belongs_to :company
   has_many :clients
   has_many :user_messages
@@ -10,4 +11,10 @@ class User < ActiveRecord::Base
          :trackable,
          :validatable,
          :registerable
+
+  def set_default_company
+    if company.blank?
+      self.company = Company.find_by(name: I18n.t('default_company_name'))
+    end
+  end
 end
