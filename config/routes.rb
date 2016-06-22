113d625ac318b288c6254cabbb232e2134828eb7
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-06-22 04:15:16 +0100

    users cannot be deleted

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,17 @@ Rails.application.routes.draw do
 
   get '/landing' => 'landing#index', as: :landing
 
-  devise_for :users
+  devise_for :users, skip: :registrations
+  devise_scope :user do
+    resource :registration,
+      only: [:new, :create, :edit, :update],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'devise/registrations',
+      as: :user_registration do
+        get :cancel
+      end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
