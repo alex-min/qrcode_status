f6755d10b9314f87cfe259bd180b03a285bfda87
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-06-22 07:49:41 +0100

    create product state for default company

--- db/seed/product_states.rb
+++ db/seed/product_states.rb
@@ -2,15 +2,21 @@ puts "[Product States]"
 
 company = User.find_by(email: ENV['ADMIN_EMAIL']).company
 
-['excellent', 'bon', 'moyen', 'mauvais'].each do |product_state|
-  if ProductState.find_by(legacy_slug: product_state).present?
-    puts "[~] ProductState #{product_state} already created..."
-  else
-    ProductState.create!(legacy_slug: product_state,
-                         name: product_state,
-                         company: company)
-    puts "[+] ProductState #{product_state} created"
+def create_product_states(company)
+  ['excellent', 'bon', 'moyen', 'mauvais'].each do |product_state|
+    if ProductState.find_by(legacy_slug: product_state, company: company).present?
+      puts "[~] ProductState #{product_state} already created..."
+    else
+      ProductState.create!(legacy_slug: product_state,
+                           name: product_state,
+                           company: company)
+      puts "[+] ProductState #{product_state} created"
+    end
   end
 end
+
+default_company_name = I18n.t('default_company_name')
+create_product_states(company)
+create_product_states(Company.find_by(name: default_company_name))
 
 puts ""
