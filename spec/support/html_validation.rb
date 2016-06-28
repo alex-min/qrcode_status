8de3956d3943775f2e15bfc5627b175aaa4f0c9b
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-06-28 07:03:12 +0100

    looks better

--- spec/support/html_validation.rb
+++ spec/support/html_validation.rb
@@ -1,21 +1,34 @@
 require 'html_validation'
 include PageValidations
+
 HaveValidHTML.show_html_in_failures = true
 HTMLValidation.ignored_attribute_errors = ['moznomarginboxes', 'mozdisallowselectionprint']
 
-Capybara::Session.class_eval do
-  alias :no_html_validation_visit :visit
-  def visit(visit_url)
-    result = no_html_validation_visit(visit_url)
+module HTMLValidationSession
+  def visit(url)
+    result = super(url)
     if driver.response_headers['Content-Type'].include?('text/html')
       html_validator = PageValidations::HTMLValidation.new
-      validation = html_validator.validation(html, visit_url)
+      validation = html_validator.validation(html, url)
       unless validation.valid?
         path = Rails.application.routes.recognize_path(current_url)
-        raise "HTML Validation Error on #{path[:controller]}/#{path[:action]}.html.erb\n"\
-              "#{validation.exceptions}"
+        output = "HTML Validation Error on #{path[:controller]}/#{path[:action]}.html.erb\n"
+        raise output + build_html_errors(html.lines, validation.exceptions)
       end
     end
-    result
+  end
+
+  private
+
+  def build_html_errors(source, exceptions)
+    error = ''
+    exceptions.lines.each do |exception|
+      line = exception.scan(/[0-9]+/).first.to_i
+      source_extract = source[line - 2..line].join('')
+      error += "#{source_extract}→ #{exception}"
+    end
+    error
   end
 end
+
+Capybara::Session.prepend(HTMLValidationSession)
