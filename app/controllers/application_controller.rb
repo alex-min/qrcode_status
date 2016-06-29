3ca7606ae8e024c0824e748a0f8b95c4ed79a5df
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-06-29 02:46:21 +0100

    loggedin helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   include Notifications
   include ClientHelpers
+
+  def logged_in?
+    current_user.present?
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
