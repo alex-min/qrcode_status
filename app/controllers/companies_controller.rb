b394f26e750e03fab4330f1fb882128e1042d5fa
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-06-29 05:08:48 +0100

    new company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,8 +1,28 @@
 class CompaniesController < ApplicationController
   include Authenticated
 
+  def authorize(params)
+    params.require(:company).permit(
+      :name,
+      :website,
+      :siret
+    )
+  end
+
   def assign
     redirect_to clients_path unless current_user.company.demo
+    if request.request_method === 'POST'
+      ActiveRecord::Base.transaction do
+        @company = Company.create!(authorize(params))
+        current_user.company = @company
+        current_user.save!
+        redirect_to clients_path
+      end
+    else
+      @company = Company.new
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    @company = e.record
   end
 
   def index
