c7c7f012eae07fb12b2a52fc0887482da3194aa8
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-07-08 07:41:28 +0100

    updating setup

--- dokku_setup.sh
+++ dokku_setup.sh
@@ -11,6 +11,6 @@ ssh "dokku@$DOKKU_HOST" postgres:create "$APPNAME"
 ssh "dokku@$DOKKU_HOST" postgres:link "$APPNAME" "$APPNAME"
 git remote add "$APPNAME" "dokku@$DOKKU_HOST:$APPNAME"
 git push "$APPNAME"
-ssh "dokku@$DOKKU_HOST" config:set --no-restart st6 DOKKU_LETSENCRYPT_EMAIL=minettealexandre@fastmail.se
+ssh "dokku@$DOKKU_HOST" config:set --no-restart "$APPNAME" DOKKU_LETSENCRYPT_EMAIL=minettealexandre@fastmail.se
 ssh "dokku@$DOKKU_HOST" letsencrypt "$APPNAME"
 
