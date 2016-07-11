3dbc417d919d648930f88f6fe1c6290ce41ef6b0
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-07-11 06:54:40 +0100

    removing all legacy deployment stuff

--- decrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for file in `find . -type f -name '*.gpg'`;
-do
-FILENOGPG=`echo $file | sed 's/.gpg//g'`
-echo gpg --decrypt "$file" --output="$FILENOGPG"
-gpg  --output="$FF"  --decrypt "$file"
-done
--- start_production.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bundle exec rake db:migrate assets:precompile
-passenger start -p 5000 -e production
