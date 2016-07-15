33eed0521ac238130da4a349d201395cbfddf3d4
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-07-15 05:46:32 +0100

    starting test

--- spec/support/default_login.rb
+++ spec/support/default_login.rb
@@ -13,4 +13,20 @@ module DefaultLogin
     fill_in :user_email, with: Faker::Internet.email
     click_button I18n.t('devise.registrations.new.sign_up_action')
   end
+
+  def signup_as_new_client_with_company
+    signup_as_new_client
+    assign_new_company
+  end
+
+  def assign_new_company
+    visit companies_assign_path
+    company = build(:company)
+    fill_in :company_name, with: company.name
+    fill_in :company_website, with: company.website
+    fill_in :company_address, with: company.address
+    fill_in :company_siret, with: company.siret
+    fill_in :company_phone, with: company.phone
+    click_button I18n.t('companies.assign.add')
+  end
 end
