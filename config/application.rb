a99aed55755415df06041285b1ff8718f7ca5f55
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-10-15 14:45:46 +0100

    fixing password reset

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,5 @@ module Status
     config.i18n.default_locale = :fr
     config.i18n.locale = 'fr'
     I18n.config.enforce_available_locales = true
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
