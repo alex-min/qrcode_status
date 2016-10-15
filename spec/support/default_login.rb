def62084c380419a20e3af183a39ec0e8d15df4e
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-10-15 15:11:09 +0100

    fixing .env

--- spec/support/default_login.rb
+++ spec/support/default_login.rb
@@ -3,7 +3,7 @@ module DefaultLogin
 
   def login_with_default_user
     visit '/users/sign_in'
-    fill_in :user_email, with: 'contact@microdeo.com'
+    fill_in :user_email, with: 'user@example.org'
     fill_in :user_password, with: '11111111'
     click_button 'Connexion'
   end
