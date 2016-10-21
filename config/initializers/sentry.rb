97f9f417b85a829976e8cc8842774a1771e01039
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-10-21 04:50:32 +0100

    adding frontend dsn

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,8 @@
 if ENV['SENTRY_DSN'].present?
   Raven.configure do |config|
     config.dsn = ENV['SENTRY_DSN']
-    config.environments = ['staging', 'production']
+    config.environments = %w(staging production)
   end
 end
+
+Rails.application.config.sentry_dsn_frontend = ENV['SENTRY_DSN_FRONTEND']
