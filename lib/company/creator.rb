f6f5bb348ed39792bf3cbd86c495eeea03cf0533
MINETTE Alexandre <minettealexandre@fastmail.se>
2016-11-12 09:04:24 +0000

    fixing company creation

--- lib/company/creator.rb
+++ lib/company/creator.rb
@@ -17,14 +17,47 @@ class Company::Creator
   private
 
   def create_base_message
-    UserMessage.create!([startrepair_message])
+    UserMessage.create!([
+          startrepair_message,
+          problem_delay_message,
+          repair_in_progress_message,
+          repair_done_message
+        ])
+  end
+
+  def repair_done_message
+    {
+      code: :repair_done,
+      title: 'Réparation terminée',
+      action: :close_ticket,
+      message: "#{hello}"\
+               "Votre <%= client.product_full_name %> est maintenant réparé, "\
+               "vous pouvez venir le chercher à tout moment au magasin."\
+               "#{horaires}"\
+                "\n#{end_message}",
+      user: @user,
+      company: @company
+    }
+  end
+
+  def repair_in_progress_message
+    {
+      code: 'repair_in_progress',
+      title: 'Réparation en cours',
+      message: "#{hello}"\
+               "Votre <%= client.product_full_name %> est en cours de réparation, "\
+               "nous vous tiendrons informés de l'avancement de la réparation.\n"\
+               "#{end_message}",
+      user: @user,
+      company: @company
+    }
   end
 
   def problem_delay_message
     {
       code: 'problem_delay',
       title: 'Problème de réception des pièces',
-      message: "#{message_hello}"\
+      message: "#{hello}"\
                "Nous avons des problèmes de réception des pièces pour votre "\
                "<%= client.product_full_name %> et la réparation est actuellement retardée.\n"\
                "Vous serez tenu informé de l'évolution de la réparation.\n"\
@@ -38,7 +71,7 @@ class Company::Creator
     {
       code: 'prise_en_charge',
       title: 'Prise en charge du produit',
-      message: "#{message_hello}\n"\
+      message: "#{hello}\n"\
                 "Votre <%= client.product_full_name %> est pris en charge.\n"\
                 "Vous serez tenu informé de l'évolution de la réparation.\n"\
                 "MESSAGE AUTOMATIQUE. MERCI DE NE PAS REPONDRE.",
@@ -47,8 +80,11 @@ class Company::Creator
     }
   end
 
+  def end_message
+    'MESSAGE AUTOMATIQUE. MERCI DE NE PAS REPONDRE.'
+  end
 
-  def basemessage_hello
+  def hello
     "<%= company.name %> - Bonjour <%= client.full_name %>."
   end
 
